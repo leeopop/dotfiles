907f594d4e7bde03bb7e4e6e1dc05b0d5ce976c5
Keunhong Lee <dlrmsghd@gmail.com>
2020-03-13 10:05:00 +0900

    Add git-remove-untracked script

---
+++ bin/git-remove-untracked.ps1
@@ -0,0 +1,9 @@
+git remote update --prune; git branch -vv | % { 
+    $branch_line = $_.toString().Replace("*","").Trim(); 
+    $branch_name = $branch_line.Replace("*","").Trim().Split(" ")[0]; 
+    $has_origin = $branch_line.Contains("[origin/");
+    $has_gone = $branch_line.Contains(": gone");
+    if ($has_gone -or -not $has_origin) {
+        git branch -d $branch_name;
+    }
+}
